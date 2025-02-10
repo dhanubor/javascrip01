703bd720fbcb953b5d52b5593ef5a2e688d728f9
dhanubor <nikhilmistry121203@gmail.com>
2025-02-10 08:53:11 +0600

    g

---
+++ condition/app.js
@@ -0,0 +1,17 @@
+const price =500;
+if (price>= 5000) {
+    const discount = price*50 / 100;
+    const pament = price - discount;
+    console.log(pament);
+} else {
+    console.log(price);
+}
+
+let number = 5;
+if (number%2 ===0) {
+    console.log( "even number");
+} else {
+    console.log('odd number');
+}
+//লগইন সিস্টেম
+let 
---
+++ condition/ternari/app.js
@@ -0,0 +1,5 @@
+let islider = true;
+let price = 200;
+price = islider === true ? 0 : price + 100;
+
+console.log(price); // আউটপুট: 0
